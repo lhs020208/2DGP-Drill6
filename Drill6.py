bface11f8c1706a298ce8eea123a5ece818c06c5
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2024-10-02 23:48:59 +0900

    캐릭터 방향 변경 추가

--- Drill6.py
+++ Drill6.py
@@ -25,7 +25,6 @@ x = 800 // 2
 y = 600 // 2
 frame = 0
 dirx = 0
-diry = 0
 
 hand_x = 800 // 2
 hand_y = 600 // 2
@@ -34,15 +33,20 @@ def set_hand_pos():
     hand_x = random.randrange(0,800)
     hand_y = random.randrange(0,600)
 def move():
-    global x,y, hand_x, hand_y
+    global x,y, hand_x, hand_y, dirx
     for i in range(0, 100 + 1, 1):
         t = i / 1500
+        previous_x = x
     if t == 1:
         x = hand_x
         y = hand_y
     else:
         x = (1 - t) * x + t * hand_x
         y = (1 - t) * y + t * hand_y
+    if x > previous_x:
+        dirx = 1
+    else:
+        dirx = 0
 def check_boy_hand_L():
     if (x >= hand_x and x - hand_x <= 20) or (x < hand_x and hand_x - x <= 20):
         if (y >= hand_y and y - hand_y <= 20) or (y < hand_y and hand_y - y <= 20):
@@ -53,10 +57,10 @@ while running:
     clear_canvas()
     grass.draw(400, 300)
 
-    if dirx < 0:
-        act = 0
-    elif dirx > 0:
+    if dirx == 1:
         act = 1
+    elif dirx == 0:
+        act = 0
 
     move()
     character.clip_draw(frame * 100, act*100, 100, 100, x, y)
